4f76fb9d8fa14342e42cb5e298e34df40476dc78
Aravind-2000 <varavind2000@gmail.com>
2022-11-27 19:50:28 +0530

    Added some API urls

--- src/Service/ApiURl's.js
+++ src/Service/ApiURl's.js
@@ -2,11 +2,19 @@ import axios from "axios";
 
 const loginurl = "http://localhost:8080/user/logindetails";
 const signupcustomer = "http://localhost:8080/user/signup";
+const signupadmin = "http://localhost:8080/user/adminsignup";
 const getAllProductsUrl = "http://localhost:8080/product/getall";
 const addProductsUrl = "http://localhost:8080/product/add";
 const getAllProdCategoryUrl = "http://localhost:8080/category/getall";
 const addProdCategoryUrl = "http://localhost:8080/category/add";
 const getAddByUserUrl = "http://localhost:8080/address/getbyuser/";
+const addEventUrl = "http://localhost:8080/event/add";
+const getAllEventUrl = "http://localhost:8080/event/getall";
+const addCouponUrl = "http://localhost:8080/coupon/add";
+const getAllCouponUrl = "http://localhost:8080/coupon/getall";
+const getMyCartUrl = "http://localhost:8080/cart/getbyuser/";
+const addProdToCartUrl = "http://localhost:8080/product/addtocart/";
+const removeProdFromCartUrl = "http://localhost:8080/product/removefromcart/";
 
 class EcommApi {
   getLoginDetails(logindetails) {
@@ -15,6 +23,10 @@ class EcommApi {
 
   doSignupAsCustomer(signupDetails) {
     return axios.post(signupcustomer, signupDetails);
+  }
+
+  doSignupAsAdmin(signupDetails) {
+    return axios.post(signupadmin, signupDetails);
   }
 
   getAllProducts() {
@@ -35,6 +47,33 @@ class EcommApi {
 
   getAddressByUser(userid) {
     return axios.get(getAddByUserUrl + userid);
+  }
+
+  addEvent(event) {
+    return axios.post(addEventUrl, event);
+  }
+
+  getAllEvent() {
+    return axios.get(getAllEventUrl);
+  }
+
+  addCoupon(coupon) {
+    return axios.post(addCouponUrl, coupon);
+  }
+  getAllCoupon() {
+    return axios.get(getAllCouponUrl);
+  }
+
+  getMyCart(userid) {
+    return axios.get(getMyCartUrl + userid);
+  }
+
+  addProductToCart(pid, cid) {
+    return axios.put(addProdToCartUrl + pid + "/" + cid);
+  }
+
+  removeProductFromCart(pid, cid) {
+    return axios.delete(removeProdFromCartUrl + pid + "/" + cid);
   }
 }
 
