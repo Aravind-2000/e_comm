ebdfe06e7596e4426d08cab67bc6f92e8e6cafa5
Aravind-2000 <varavind2000@gmail.com>
2022-11-27 19:48:56 +0530

    Added routes for cart and other routes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import ProductsList from "./Components/ProductsList";
 import Signup from "./Components/Signup";
-import NavBar from "./Components/NavBar";
 import LoginDetails from "./Components/LoginDetails";
+import ResponsiveAppBar from "./Components/AppBar";
+import MyCart from "./Components/MyCart";
+import AdminSignup from "./Components/Admin/AdminSignup";
+import Checkout from "./Components/Checkout";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <ResponsiveAppBar />
       <div>
         <br /> <br />
         <Routes>
-          <Route exact path={"/"} element={<ProductsList />} />
+          <Route exact path={"/products"} element={<ProductsList />} />
           <Route exact path={"/login"} element={<Login />} />
           <Route exact path={"/signup"} element={<Signup />} />
+          <Route exact path={"/adminsignup"} element={<AdminSignup />} />
           <Route exact path={"logindetails"} element={<LoginDetails />} />
+          <Route exact path={"/mycart"} element={<MyCart />} />
+          <Route exact path={"/checkout"} element={<Checkout />} />
         </Routes>
       </div>
     </div>
