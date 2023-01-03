70d33467876533ef851e256f2dc8a3f7b16c3a0f
Aravind-2000 <varavind2000@gmail.com>
2023-01-03 20:41:04 +0530

    Added comparision for start date and end date

--- src/Components/AddEvent.js
+++ src/Components/AddEvent.js
@@ -2,7 +2,14 @@ import React, { useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { Box, Grid, FormControl, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Grid,
+  FormControl,
+  Button,
+  TextField,
+  FormHelperText,
+} from "@mui/material";
 import dayjs from "dayjs";
 import ApiURlS from "../Service/ApiURl's";
 
@@ -11,26 +18,43 @@ const AddEvent = ({ close }) => {
   const [eventstartdate, seteventStartDate] = useState(null);
   const [eventenddate, seteventEndDate] = useState(null);
 
+  const [dateErrorText, setdateErrorText] = useState("");
+  const compareDates = (date1, date2) => {
+    if (dayjs(date1).isBefore(dayjs()) || dayjs(date2).isBefore(dayjs())) {
+      setdateErrorText("Start date or end date is before current date");
+      return false;
+    } else {
+      if (dayjs(date1).isAfter(date2)) {
+        setdateErrorText("Start date is after the end date");
+        return false;
+      }
+    }
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const eventStartDate = dayjs(eventstartdate).format("MM-DD-YYYY");
     const eventEndDate = dayjs(eventenddate).format("MM-DD-YYYY");
-    const body = {
-      eventName,
-      eventStartDate,
-      eventEndDate,
-    };
-    ApiURlS.addEvent(body)
-      .then((res) => {
-        console.log(res.data);
-        close();
-      })
-      .catch((err) => console.log(err));
+    if (compareDates(eventStartDate, eventEndDate)) {
+      const body = {
+        eventName,
+        eventStartDate,
+        eventEndDate,
+      };
+      // console.log(body)
+      ApiURlS.addEvent(body)
+        .then((res) => {
+          // console.log(res.data);
+          close();
+        })
+        .catch((err) => console.log(err));
+    }
   };
 
   return (
     <div>
-      <form autoComplete="off">
+      <form onSubmit={handleSubmit} autoComplete="off">
         <Box sx={{ flexGrow: 1 }}>
           <Grid container spacing={2}>
             <Grid item xs={8} md={6} lg={4}>
@@ -42,6 +66,7 @@ const AddEvent = ({ close }) => {
                 placeholder="Enter Event Name "
                 value={eventName}
                 label="Event Name"
+                required
                 onChange={(e) => seteventName(e.target.value)}
               />
             </Grid>
@@ -65,6 +90,10 @@ const AddEvent = ({ close }) => {
                   />
                 </LocalizationProvider>
               </FormControl>
+              <FormHelperText sx={{ fontSize: "19px" }} error>
+                {" "}
+                {dateErrorText === null ? null : dateErrorText}{" "}
+              </FormHelperText>
             </Grid>
             <Grid item xs={8} md={6} lg={4}>
               <FormControl
@@ -90,7 +119,8 @@ const AddEvent = ({ close }) => {
           </Grid>
           <br />
           <Button
-            onClick={(e) => handleSubmit(e)}
+            // onClick={(e) => handleSubmit(e)}
+            type="submit"
             variant="contained"
             color="success"
           >
